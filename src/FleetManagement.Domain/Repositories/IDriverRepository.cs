3e1bfd9258388cd1f5890c7dd732d67e46d374d1
Rilary Gomes <70041774+rilarygomes@users.noreply.github.com>
2026-08-12 21:57:14 -0300

    feat(domain) #3: add repository contracts
    - Define IVehicleRepository with CRUD operations and lookup by license plate
    - Define IDriverRepository with CRUD operations and lookup by license number
    - Define ITripRepository with CRUD operations and queries by vehicle and driver

---
+++ src/FleetManagement.Domain/Repositories/IDriverRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace FleetManagement.Domain.Repositories
+{
+    using FleetManagement.Domain.Entities;
+
+    public interface IDriverRepository
+    {
+        Driver? GetById(Guid id);
+        Driver? GetByLicenseNumber(string licenseNumber);
+        IEnumerable<Driver> GetAll();
+        void Add(Driver driver);
+        void Update(Driver driver);
+        void Remove(Guid id);
+    }
+}
---
+++ src/FleetManagement.Domain/Repositories/IVehicleRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace FleetManagement.Domain.Repositories
+{
+    using FleetManagement.Domain.Entities;
+
+    public interface IVehicleRepository
+    {
+        Vehicle? GetById(Guid id);
+        Vehicle? GetByLicensePlate(string licensePlate);
+        IEnumerable<Vehicle> GetAll();
+        void Add(Vehicle vehicle);
+        void Update(Vehicle vehicle);
+        void Remove(Guid id);
+    }
+}
